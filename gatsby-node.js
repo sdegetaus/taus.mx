2986ac477ecdf57a41c0e40c1632da10b7167c50
Santiago Degetau Sada <santiago.degetau@icloud.com>
2020-07-05 13:37:02 -0600

    Fresh build with no [hash] in filenames

--- gatsby-node.js
+++ gatsby-node.js
@@ -18,3 +18,24 @@ module.exports.onCreateNode = ({ node, actions }) => {
     });
   }
 };
+
+// 1. don't build with source maps
+// 2. remove [hash] from filenames
+exports.onCreateWebpackConfig = (
+  { stage, getConfig, actions },
+  pluginOptions
+) => {
+  if (stage === "build-javascript") {
+    const newWebpackConfig = {
+      ...getConfig(),
+      output: {
+        filename: `[name].js`,
+        chunkFilename: `[name].js`,
+        path: getConfig().output.path,
+        publicPath: getConfig().output.publicPath,
+      },
+      devtool: false,
+    };
+    actions.replaceWebpackConfig(newWebpackConfig);
+  }
+};
