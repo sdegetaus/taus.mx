3233cbcc968ab5867f755bdec3eb216c1cc526fe
Santiago Degetau Sada <santiago.degetau@icloud.com>
2020-07-05 18:47:07 -0600

    Gatsby Intl: don't redirect

--- gatsby-config.js
+++ gatsby-config.js
@@ -118,7 +118,7 @@ module.exports = {
         path: `${__dirname}/src/intl`,
         languages: ["en", "es"],
         defaultLanguage: "en",
-        redirect: true,
+        redirect: false,
       },
     },
     {
