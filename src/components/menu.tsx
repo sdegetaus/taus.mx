3615aa7e7fa3537599507571386598ddaf5c6ac9
Santiago Degetau Sada <santiago.degetau@icloud.com>
2020-07-07 07:04:35 -0600

    Remove home button

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -9,16 +9,6 @@ const Menu = ({ intl }: MenuProps) => {
     <div className={`${styles.menu}`}>
       <nav>
         <ul>
-          <li className={`${styles.item}`}>
-            <Link
-              to={`/`}
-              activeClassName={styles.active}
-              title={intl.formatMessage({ id: "pages.home" })}
-              rel={"noopener"}
-            >
-              <span className="fa fa-home"></span>
-            </Link>
-          </li>
           <li className={`${styles.item} ${styles.language}`}>
             <Language />
           </li>
